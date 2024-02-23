1985826cacfa92d5fe064aae3e69b7c8d2abbb76
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-23 15:53:28 -0300

    fix: create remote-entry files to expose remotes to federation

--- pnpm-monorepo-nx-react/apps/host/module-federation.config.ts
+++ pnpm-monorepo-nx-react/apps/host/module-federation.config.ts
@@ -3,6 +3,9 @@ import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'host',
   remotes: ['product'],
+  exposes: {
+    './Host': './src/remote-entry.ts',
+  },
 };
 
 export default config;
