dc13e44288a7e94c31c072b6f14bb6dee89b36b0
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-23 18:07:06 -0300

    feat(host): add product

--- pnpm-monorepo-nx-react/apps/host/src/app/app.tsx
+++ pnpm-monorepo-nx-react/apps/host/src/app/app.tsx
@@ -1,14 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import NxWelcome from './nx-welcome';
 
-const Product = lazy(() => import('product/Product'));
+const Product = lazy(() => import('product/Module'));
 
 export function App() {
   return (
     <div>
       <NxWelcome title="host" />
+      <Suspense fallback="Loading Product">
+        <Product />
+      </Suspense>
     </div>
   );
 }
