b3462a30f7d00c1149f862de7957640588168f71
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-23 17:04:55 -0300

    chore(host): add product as implicit remote

--- pnpm-monorepo-nx-react/apps/host/src/app/app.tsx
+++ pnpm-monorepo-nx-react/apps/host/src/app/app.tsx
@@ -1,6 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 
+import { lazy } from 'react';
 import NxWelcome from './nx-welcome';
+
+const Product = lazy(() => import('product/Product'));
 
 export function App() {
   return (
