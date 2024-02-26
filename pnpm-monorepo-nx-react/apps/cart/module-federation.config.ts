e3c30d292109b894a1afc153f701eaac3bc652b2
Priscila Andreani <priscila.andreani@grupoboticario.com.br>
2024-02-26 16:41:03 -0300

    feat: create new remote cart

--- pnpm-monorepo-nx-react/apps/cart/module-federation.config.ts
+++ pnpm-monorepo-nx-react/apps/cart/module-federation.config.ts
@@ -1,12 +1,11 @@
 import { ModuleFederationConfig } from '@nx/webpack';
 
 const config: ModuleFederationConfig = {
-  name: 'host',
+  name: 'cart',
   exposes: {
     // map to the internal file path
     './Module': './src/remote-entry.ts',
   },
-  remotes: ['product', 'cart'],
 };
 
 export default config;
--- pnpm-monorepo-nx-react/apps/host/src/app/app.tsx
+++ pnpm-monorepo-nx-react/apps/host/src/app/app.tsx
@@ -3,13 +3,15 @@ import NxWelcome from './nx-welcome';
 
 // build time remote import
 const Product = lazy(() => import('product/Module'));
+const Cart = lazy(() => import('cart/Module'));
 
 export function App() {
   return (
     <div>
       <NxWelcome title="host" />
-      <Suspense fallback="Loading Product">
+      <Suspense fallback="Loading remotes">
         <Product />
+        <Cart />
       </Suspense>
     </div>
   );
